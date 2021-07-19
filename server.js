e275a78328f532a41e58d9f91ca778fd15fd25d6
root <root@ip-172-31-36-98.ap-south-1.compute.internal>
2021-07-19 04:50:26 +0000

    4th commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function(req,res){
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello vikky!');
+        res.end('Hello vikky, Where are you?');
 }).listen(8080);
 console.log('Server started on localhost:8080; press Ctrl-C to terminate...!');
